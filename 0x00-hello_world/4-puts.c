70e02fe14a828b211f1c35a343bf8052515d8fb1
Lucky-Willis Mwabi <l.willismwabi@gmail.com>
2023-10-09 14:50:19 -0700

    Task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*Main - entry point
+*return: always 0 (success)
+*/
+int main(void)
+{
+        puts("\"Programminng is like building a multilingual puzzle");
+        return (0);
+}
